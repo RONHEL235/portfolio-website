3b49af0a0b6379439be83da7aed23224983819e5
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-04-23 12:10:18 +0200

    Created a projects component to display all projects.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Intro from "@/components/intro"
-import About from "@/components/about"
 import SectionDivider from "@/components/section-divider"
+import About from "@/components/about"
+import Projects from "@/components/projects"
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
+      <Projects />
     </main>
   )
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -33,7 +33,7 @@ export default function Intro() {
                     </motion.div>
                 </div>
             </div>
-            <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+            <motion.h1 className="mb-10 mt-10 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
                 initial={{ opacity: 0, y: 100 }}
                 animate={{ opacity: 1, y: 0}}
             >
