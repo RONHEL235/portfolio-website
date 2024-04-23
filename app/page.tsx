7b8aa440e345002aca9bace66ab61a942f323192
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-04-23 10:07:52 +0200

    Created an about component that details further information about me.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Intro from "@/components/intro"
+import About from "@/components/about"
 import SectionDivider from "@/components/section-divider"
+
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider />
+      <About />
     </main>
   )
 }
