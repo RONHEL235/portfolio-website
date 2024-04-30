34547bfc311031d01a2967351ae60f4620bbd9d8
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-04-30 11:34:31 +0200

    Used useEffect to delay transition on header links temporarily so as to allow smooth scrolling on links.

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,13 +10,13 @@ export default function Projects() {
   const { ref, inView } = useInView({
     threshold: 0.65,
   })
-  const { setActiveSection } = useActiveSectionContext()
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
 
   useEffect(() => {
-    if (inView) {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
     setActiveSection("Projects")
   }
-}, [inView, setActiveSection]) 
+}, [inView, setActiveSection, timeOfLastClick]) 
 
 
   return (
--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -12,18 +12,23 @@ type ActiveSectionContextProviderProps = {
 type ActiveSectionContextType = {
     activeSection: SectionName
     setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>
+    timeOfLastClick: number
+    setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>
 }
 
 export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null)
 
 export default function ActiveSectionContextProvider({ children }: ActiveSectionContextProviderProps) {
     const [activeSection, setActiveSection] = useState<SectionName>("Home")
+    const [timeOfLastClick, setTimeOfLastClick] = useState<number>(0)
 
   return (
     <ActiveSectionContext.Provider 
     value={{
         activeSection,
-        setActiveSection
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick
     }}>
         {children}
     </ActiveSectionContext.Provider>
