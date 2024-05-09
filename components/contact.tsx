ffc1f9f9f1ed82eda2dfbaa0069b25de7bf2e7d7
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-05-09 11:10:46 +0200

    Added a loader to the form submit button to indicate when a message is sent.

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,10 @@
 "use client"
 
 import React from 'react'
-import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from "framer-motion"
 import { useSectionInView } from '@/lib/hooks'
 import { sendEmail } from '@/actions/sendEmail'
-
+import SubmitBtn from "./submit-btn"
 
 export default function Contact() {
   const {ref} = useSectionInView("Contact", 0.99)
@@ -52,15 +51,7 @@ export default function Contact() {
       required={true}
       maxLength={5000} 
     />
-      <button 
-        className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all
-        focus:scale-110 hover:scale-110 hover:bg-gray active:scale-105"
-        type="submit">Submit 
-        <FaPaperPlane 
-        className="text-xs opacity-70 transition-all
-        group-hover:translate-x-1
-        group-hover:-translate-y-1" />{" "}
-      </button>
+    <SubmitBtn />
     </form>
     </motion.section>
   )
---
+++ components/submit-btn.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useFormStatus } from 'react-dom'
+import { FaPaperPlane } from 'react-icons/fa'
+
+export default function SubmitBtn() {
+    const { pending } = useFormStatus()
+
+  return (
+    <button 
+        className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all
+        focus:scale-110 hover:scale-110 hover:bg-gray active:scale-105 disabled:scale-100 disabled:bg-opacity-75"
+        type="submit"
+        disabled={pending}
+        >
+        {pending? (<div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>) : (
+            <>
+                Submit 
+                <FaPaperPlane
+                className="text-xs opacity-70 transition-all
+                group-hover:translate-x-1
+                group-hover:-translate-y-1" />{" "}  
+            </>
+        )}
+      </button>
+  )
+}
