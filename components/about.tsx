dfdd922df99a8393e682cf69b0f6d06becbbc74d
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-05-22 12:05:40 +0200

    Styling the projects components on dark mode.

--- components/about.tsx
+++ components/about.tsx
@@ -10,34 +10,34 @@ export default function About() {
   return (
     <motion.section 
     ref={ref}
-    className="mt-[17rem] mb-[7rem] max-w-[45rem] text-center leading-8 scroll-mt-28"
+    className="mt-[17rem] mb-[7rem] max-w-[45rem] text-center leading-8 scroll-mt-28 font-medium"
     initial={{ opacity: 0, y: 100 }}
     animate={{ opacity: 1, y: 0}}
     transition={{ delay: 0.175 }}
     id="about"
     >
-        <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
+        <h2 className="text-3xl font-semibold capitalize mb-8">About me</h2>
         <p className="mb-3">
         After my incomplete 4 year tenure at {" "}
-        <span className="font-medium">UCT - BSc Geomatics (Land Surveying) due to financial exclusion</span>, I decided to pursue my
+        <span className="font-semibold">UCT - BSc Geomatics (Land Surveying) </span>due to financial exclusion, I decided to pursue my
         passion for programming and enrolled in a coding bootcamp called CodeSpace and learned{" "}
-        <span className="font-medium">Front-End Software development</span>.{" "}
+        <span className="font-semibold">Front-End Software development</span>.{" "}
         </p>
         <p>
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
-        <span className="font-medium">Tailwind CSS, JavaScript, React.js and Node.js. </span>
+        <span className="font-semibold">Tailwind CSS, JavaScript, React.js and Node.js. </span>
         I am also familiar with 
-        <span className="font-medium"> TypeScript. </span>
+        <span className="font-semibold"> TypeScript. </span>
         Presently I am in my first year pursuing a 
-        <span className="font-medium"> BSc in IT specializing in Software Engineering at EDUVOS. </span> 
+        <span className="font-semibold"> BSc in IT specializing in Software Engineering at EDUVOS. </span> 
         I am currently looking for a{" "}
-        <span className="font-medium">internship position as a Front-End software developer.</span>
+        <span className="font-semibold">internship position as a Front-End software developer.</span>
         </p>
         <p className="mt-5">
-        <span className="italic">When I'm not coding</span>, I like to delve into a good fictional read or personal writing. I love learning, and presently I am learning about philosophy and history. 
+        <span className="italic">When I'm not coding</span>, I like to delve into a good fictional read or personal writing. <span className="font-semibold">I love learning and constantly seeking out challenges and opportunities that aid my personal and intellectual growth.</span> 
         </p>    
     </motion.section>
   )
--- components/project.tsx
+++ components/project.tsx
@@ -29,10 +29,10 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
         className="bg-gray-100 max-w-[42rem] border border-black/10 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0  hover:bg-gray-300 transition group-even:pl-[3.5rem] dark dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+        <p className="mt-2 leading-relaxed font-medium">{description}</p>
         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
-        <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>{tag}</li>
+        <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70" key={index}>{tag}</li>
       ))}
     </ul>
     </div>
