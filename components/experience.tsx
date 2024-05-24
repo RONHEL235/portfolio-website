6dd73a6d6a20e6cbf132679da1f68ad916ed7053
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-05-24 09:14:51 +0200

    Styled Experience component in dark mode.

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,10 +5,11 @@ import { experiencesData } from '@/lib/data'
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
 import "react-vertical-timeline-component/style.min.css"
 import { useSectionInView } from '@/lib/hooks'
+import { useTheme } from '@/context/theme-context'
 
 export default function Experience() {
     const { ref } = useSectionInView("Experience", 0.65)
-    useTheme
+    const { theme } = useTheme()
 
   return (
     <section 
@@ -16,31 +17,31 @@ export default function Experience() {
         id="experience"
         className="scroll-mt-28 mb-28 sm:mb-40">
         <div className="text-3xl font-medium capitalize mb-8 text-center">Experience</div>
-        <VerticalTimeline>
+        <VerticalTimeline lineColor= "">
             {experiencesData.map((item, index) => (
                 <React.Fragment key={index}>            
                     <VerticalTimelineElement
                     visible={true}
                     contentStyle={{
-                        background: "#f3f4f6",
+                        background: theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                         boxShadow: "none",
                         border: "1px solid rgba(0, 0, 0, 0.05)",
                         textAlign: "left",
                         padding: "1.3rem 2rem"
                     }}
                     contentArrowStyle={{
-                        borderRight: "0.4rem solid #9ca3af",
+                        borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255, 255, 255, 0.5)",
                     }}
                     date={item.date}
                     icon={item.icon}
                     iconStyle={{
-                        background: "white",
+                        background: theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                         fontSize: "1.5rem",
                     }}
                     >
                         <h3 className="font-semibold capitalize">{item.title}</h3>
                         <p className="font-normal !mt-0">{item.location}</p>
-                        <p className="!mt-1 !font-normal text-gray-700">{item.description}</p>
+                        <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">{item.description}</p>
                     </VerticalTimelineElement>
                 </React.Fragment>
             ))}
