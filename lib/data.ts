709a4d3f1b42edeb3fd749ec09d8e1befcc53bed
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-10-28 19:47:48 +0200

    The pictures are failing to display, still fixing the bug.

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import theMidnightPodcast from "@/public/The_Midnight_Podcast.PNG";
+import theMidnightPodcast from "@/public/";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
