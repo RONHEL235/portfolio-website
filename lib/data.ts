5d2f4ab8e344c101a934acfc380a1923093650c1
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-10-30 19:51:01 +0200

    Added the image to my second project.

--- lib/data.ts
+++ lib/data.ts
@@ -3,7 +3,7 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import theMidnightPodcast from "@/public/The_Midnight_Podcast.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import instascape from "@/public/Instascape.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
