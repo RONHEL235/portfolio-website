877dc10d261e9bf00a6ecb5417ef96e389d12de9
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-12-08 20:14:25 +0200

    Extracting a readable error message from the error object.

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -43,7 +43,7 @@ export const sendEmail = async (formData: FormData) => {
   } catch (error: unknown) {
     // If an error occurs while sending the email, handle it gracefully.
     return {
-      error: getErrorMessage(error),
+      error: getErrorMessage(error),// Extract a readable error message from the error object.
     }
   }
    return {
